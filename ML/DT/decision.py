bb9a1ee65e73dde17181a96bc1da7ab0bd191a37
Viswesh Subramanian <86516835+Gviswesh@users.noreply.github.com>
2023-05-03 21:47:20 +0530

    Addition of Decision Tree exercise

---
+++ ML/DT/decision.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split as tts
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import ConfusionMatrixDisplay, accuracy_score
+
+data = pd.read_csv('iris.csv', index_col = 'Id')
+data.head()
+
+features = ['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm']
+X = data[features]
+y = data['Species']
+
+x_train, x_test, y_train, y_test = tts(X, y, test_size=0.25, random_state = 1)
+
+x_train.describe()
+
+# Decision tree classifier
+model = DecisionTreeClassifier()
+model.fit(x_train, y_train)
+
+y_pred = model.predict(x_test)
+accuracy_score(y_test, y_pred)
